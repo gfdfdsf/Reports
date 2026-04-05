31256fefa6af9639bc97377e06abf4b271fc043c
Sebastian Perez <164391381+gfdfdsf@users.noreply.github.com>
2026-04-05 18:37:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Proyecto_Integrador_DevOps.Infrastructure;
+using Proyecto_Integrador_DevOps.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession();
+
+builder.Services.AddScoped<ReportDAO>();
+builder.Services.AddScoped<ReportService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
+
+app.Run();
